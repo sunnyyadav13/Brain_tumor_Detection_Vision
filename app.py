2db81dcc89902884237e77dcb31a7e91abac1deb
sunnyyadav13 <sunnyyadav13091999@gmail.com>
2022-03-21 23:04:12 +0530

    firstcommit

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import cv2
+from tensorflow.keras.models import load_model
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+
+model =load_model('vgg19_final.h5')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def get_className(classNo):
+	if classNo==0:
+		return "No Brain Tumor"
+	elif classNo==1:
+		return "Yes Brain Tumor"
+
+
+def getResult(img):
+    Size=64
+    image=cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((Size,Size))
+    image=np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result=model.predict_classes(input_img)
+    return result
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        value=getResult(file_path)
+        result=get_className(value) 
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
